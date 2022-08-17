2851abf3b32003f66795142b1e5d101ab95d9851
Generic User <generic@user.net>
2022-08-17 11:17:55 -0400

    changed to new  api structure

--- gtmarket/__init__.py
+++ gtmarket/__init__.py
@@ -1,5 +1,23 @@
-from .version import __version__
-from .api import *
+# flake8: noqa
+from .api import API, Importable
 
-__all__ = ['version', 'gtm']
+# This object is loaded with descriptors that lazy import items from modules
+api_obj = API()
 
+# This list will hold all the items the package will export
+dir_list = []
+
+# We don't actually define any of the import in the namespace.
+# We rely on this function to lazy load them
+def __getattr__(name):
+    return getattr(api_obj, name)
+
+# We cycle over the api items and add their names to the exportable namespace
+for att_name, att_obj in API.__dict__.items():
+    if isinstance(att_obj, Importable):
+        dir_list.append(att_name)
+
+
+# (I)Python looks at this function to determine what is exported
+def __dir__():
+    return dir_list
--- gtmarket/api.py
+++ gtmarket/api.py
@@ -1,43 +1,35 @@
-import importlib
-import sys
-
-"""
-This module is a little funkey, but here's why it exists the way it does.
-
-I want a module that lazy loads all the submodules.  It'll frequently be the case
-that required libaries in those submodules are not installed, and I don't want to 
-barf the whole package import just because those modules are not installed when all
-I need a a tool that doesn't require them.
-
-So the strategy here is to create a class with lazy-loading attributes that themselves
-import the classes I want.
-
-I could have tried to use getattr on a module as detailed here, but I wanted autocomplete
-in jupyter, and couldn't figure out how to do that.
-https://stackoverflow.com/questions/2447353/getattr-on-a-module
-"""
-
-
 class Importable:
     """
     This is a descriptor that knows how to load modules
     """
-    def __init__(self, module_path, artifact_name):
+    def __init__(self, module_path, artifact_name, instantiate=False):
+        self.instantiate = instantiate
         self._module_path = module_path
         self._artifact_name = artifact_name
 
     def __get__(self, instance, owner):
+        # Import the module
+        import importlib
         module = importlib.import_module(self._module_path)
-        return getattr(module, self._artifact_name)
+
+        # Get the item (class/function/object) from the module
+        thing = getattr(module, self._artifact_name)
+
+        # If an object was requested, instantiate it
+        if self.instantiate:
+            thing = thing()
+        return thing
 
     def __set__(self, instance, value):
         raise NotImplementedError('You cannot set this attribute')
 
 
-class GTM:
+class API:
     """
     Every object in the api is lazy loaded through a descriptor.  The actual
-    attributes returned are classes.
+    attributes returned are classes/functions unless the instantiate flag is set.
+
+    The lazy loading ensures that you only run imports if they are needed.
     """
     OrderProducts = Importable('gtmarket.core', 'OrderProducts')
     OppLoader = Importable('gtmarket.core', 'OppLoader')
@@ -48,14 +40,3 @@ class GTM:
     ModelParamsHist = Importable('gtmarket.predictor', 'ModelParamsHist')
     SDRTeam = Importable('gtmarket.predictor', 'SDRTeam')
     Deals = Importable('gtmarket.predictor', 'Deals')
-
-    # This allows from gtmarket.gtm import * to be used
-    __all__ = list(set(vars().keys()) - {'__module__', '__qualname__', 'importlib', 'sys', 'Importable'})
-
-
-# Instantiate a GTM object
-gtm = GTM()
-
-# Add it do the modules dict so that it can be imported with module syntax
-sys.modules['gtmarket.gtm'] = gtm
-__all__ = ['gtm']
